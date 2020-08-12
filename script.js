823c2772a1957d5b8130eede680a5f4b9842cf6f
Shaker Kattoula <smkattoula@gmail.com>
2020-08-12 16:25:55 -0700

    create validations for guessing a number

--- script.js
+++ script.js
@@ -16,8 +16,39 @@ recognition.start();
 function onSpeak(e) {
   const msg = e.results[0][0].transcript;
 
-  //   writeMessage(msg);
-  //   checkNumber(msg);
+  writeMessage(msg);
+  checkNumber(msg);
+}
+
+// Create a function that writes what the user speaks
+function writeMessage(msg) {
+  msgEL.innerHTML = `<div>You said: </div>
+    <span class="box">${msg}</span>`;
+}
+
+// Create a function that checks msg against number
+function checkNumber(msg) {
+  const num = +msg;
+
+  // Check if valid number
+  if (Number.isNaN(num)) {
+    return (msgEL.innerHTML += "<div>That is not a valid number</div>");
+  }
+
+  // Check if in range
+  if (num > 100 || num < 1) {
+    return (msgEL.innerHTML += "<div>Number must be between 1 - 100</div>");
+  }
+
+  // Check random number
+  if (num === randomNum) {
+    document.body.innerHTML = `<h2>Congratulations! You've guessed the correct number! <br><br> It was ${num}</h2>
+      <button class="play-again" id="play-again">Play Again</button>`;
+  } else if (num > randomNum) {
+    msgEL.innerHTML += "<div>GO LOWER</div>";
+  } else {
+    msgEL.innerHTML += "<div>GO HIGHER</div>";
+  }
 }
 
 // Create a function that generates a random number
@@ -27,3 +58,12 @@ function getRandomNumber() {
 
 // Speak result
 recognition.addEventListener("result", onSpeak);
+
+// End Speech Recognition service
+recognition.addEventListener("end", () => recognition.start());
+
+document.body.addEventListener("click", (e) => {
+  if (e.target.id === "play-again") {
+    window.location.reload();
+  }
+});
